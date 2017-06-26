77ec493861fd21298b66ec687bfb298bd75294ef
Nick McKendry <nickmckendry@gmail.com>
2017-06-26 13:17:38 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,33 +1,28 @@
-# yourspace
+# YourSpace
 
+YourSpace is a dashboard for any sort of cooperative work space. An admin can go in and setup user accounts and add rooms to the database and manage any sort of info or data they want to store on users or rooms.
 
-getRooms(){
-  return [
-    {name: 'Kirby', seats: 6, tv: false, floor:'Lower Level'},
-    {name: 'Pikachu', seats: 6, tv: true, floor:'Lower Level'},
-    {name: 'Banana', seats: 6, tv: false, floor:'Lower Level'},
-    {name: 'Mortal Kombat', seats: 12, tv: false, floor:'Lower Level'},
-    {name: 'Pacman', seats: 6, tv: true, floor:'Lower Level'},
-    {name: 'Triforce', seats: 12, tv: false, floor:'Lower Level'}
-    {name: 'Bowser', seats: 6, tv: true, floor:'Third Floor'}
-    {name: 'One Up', seats: 12, tv: true, floor:'Third Floor'}
-    {name: 'Peach', seats: 8, tv: true, floor:'Third Floor'}
-    { name: 'Sonic', seats: 6, tv: true, floor:'Third Floor'}
-    { name: 'Gold Coin', seats: 12, tv: true, floor:'Fourth Floor'}
-    { name: 'Mega Man', seats: 6, tv: true, floor:'Fourth Floor'}
-  ]
-}
+# Deployment
+Head to: https://yourspce.herokuapp.com/
 
-# TODO #
-[ ] styling
-[ ] description in collection
-[ ] Make admin actual login
-[ ] track users across views
-[ ] MOMENT
-[ ] fix unlimited seed in database
-[ ] figure out seed issue with heroku
-[ ] change tv to yes or no
-[ ] how to link back to app from edit page
+Users will be loaded onto a page that is prototyped for the Galvanize Platte Campus. In the top left corner you can create an account and login. You will then be able to click on each room separately and view more info about it. On the page we have a prototype of what room reservation would look like. In the top right you can click on admin and see a prototype of what the admin portal would look like. From here you can Create, Update, Edit, or Delete rooms and user info.
 
-##Product description##
-YourSpace is a better way to track the spaces you have and 
+## Built with
+
+* React
+* Meteor
+* MongoDB
+* Blaze
+* React-Router v4
+
+## Authors
+
+* **Nick McKendry** - *Full Stack* - [github handle](https://github.com/NickMcKendry)
+
+* **James Schultz** - *Full Stack* - [github handle](https://github.com/ninjames101)
+
+* **Bradley Gillick** - *Full Stack* - [github handle](https://github.com/bradleygillick)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- server/main.js
+++ server/main.js
@@ -5,26 +5,26 @@ import { Times } from '../imports/api/times.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
-//   Rooms.insert({name: 'Kirby', seats: 6, tv: "No", floor:'Lower Level',
-// })
-//   Rooms.insert({name: 'Pikachu', seats: 6, tv: "Yes", floor:'Lower Level'})
-//   Rooms.insert({name: 'Banana', seats: 6, tv: "No", floor:'Lower Level'})
-//   Rooms.insert({name: 'Mortal Kombat', seats: 12, tv: "No", floor:'Lower Level'})
-//   Rooms.insert({name: 'Pacman', seats: 6, tv: "Yes", floor:'Lower Level'})
-//   Rooms.insert({name: 'Triforce', seats: 12, tv: "No", floor:'Lower Level'})
-//   Rooms.insert({name: 'Bowser', seats: 6, tv: "Yes", floor:'Third Floor'})
-//   Rooms.insert({name: 'One Up', seats: 12, tv: "Yes", floor:'Third Floor'}
-// )
-//   Rooms.insert({name: 'Peach', seats: 8, tv: "Yes", floor:'Third Floor'})
-//   Rooms.insert({ name: 'Sonic', seats: 6, tv: 1, floor:'Third Floor'}
-// )
-//   Rooms.insert({ name: 'Gold Coin', seats: 12, tv: "Yes", floor:'Fourth Floor'}
-// )
-//   Rooms.insert({ name: 'Mega Man', seats: 6, tv: "Yes", floor:'Fourth Floor'}
-// )
+  Rooms.insert({name: 'Kirby', seats: 6, tv: "No", floor:'Lower Level',
+})
+  Rooms.insert({name: 'Pikachu', seats: 6, tv: "Yes", floor:'Lower Level'})
+  Rooms.insert({name: 'Banana', seats: 6, tv: "No", floor:'Lower Level'})
+  Rooms.insert({name: 'Mortal Kombat', seats: 12, tv: "No", floor:'Lower Level'})
+  Rooms.insert({name: 'Pacman', seats: 6, tv: "Yes", floor:'Lower Level'})
+  Rooms.insert({name: 'Triforce', seats: 12, tv: "No", floor:'Lower Level'})
+  Rooms.insert({name: 'Bowser', seats: 6, tv: "Yes", floor:'Third Floor'})
+  Rooms.insert({name: 'One Up', seats: 12, tv: "Yes", floor:'Third Floor'}
+)
+  Rooms.insert({name: 'Peach', seats: 8, tv: "Yes", floor:'Third Floor'})
+  Rooms.insert({ name: 'Sonic', seats: 6, tv: 1, floor:'Third Floor'}
+)
+  Rooms.insert({ name: 'Gold Coin', seats: 12, tv: "Yes", floor:'Fourth Floor'}
+)
+  Rooms.insert({ name: 'Mega Man', seats: 6, tv: "Yes", floor:'Fourth Floor'}
+)
 
-  //
-  // });
+
+  });
 
   // Times.insert({ timeslot1: "12 am", timeslot2: "1 am", timeslot3: "2 am", timeslot4: "3 am", timeslot4: "4 am", timeslot5: "5 am", timeslot6: "6 am", timeslot7: "7 am", timeslot8: "8 am", timeslot9: "9 am", timeslot10: "10 am", timeslot11: "11 am", timeslot12: "12 pm", timeslot13: "1 pm", timeslot14: "2 pm", timeslot15: "3 pm", timeslot16: "4 pm", timeslot17: "5 pm", timeslot17: "6 pm", timeslot18: "7 pm", timeslot19: "8 pm", timeslot20: "9 pm", timeslot21: "10 pm", timeslot22: "11 pm"})
 
